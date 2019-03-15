734ec0a4648a0fd43af9d4dc088fdc784150451d
A.D.G.Sankar <a.d.g.sankar@gmail.com>
2019-03-15 16:51:53 +0530

    converted a normal object into a entity using jpa

--- src/main/java/com/example/database/databasedemo/entity/Person.java
+++ src/main/java/com/example/database/databasedemo/entity/Person.java
@@ -2,8 +2,21 @@ package com.example.database.databasedemo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name="person")  we don't need this because the table name is matching with the class name
 public class Person {
+
+	@Id //To specify primary key
+	@GeneratedValue //To assign id automatically.
 	private int id;
+	
+//	@Column(name="name") we don't need this because the column name is matching with the field name
 	private String name;
 	private String location;
 	private Date birthDate;
@@ -16,6 +29,15 @@ public class Person {
 		this.location = location;
 		this.birthDate = birthDate;
 	}
+	
+	public Person(String name, String location, Date birthDate) {
+		super();
+		this.name = name;
+		this.location = location;
+		this.birthDate = birthDate;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Person [id=" + id + ", name=" + name + ", location=" + location + ", birthDate=" + birthDate + "]";
