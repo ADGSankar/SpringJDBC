d9df27608e44884a2f569b91540c2d19b59c1963
A.D.G.Sankar <a.d.g.sankar@gmail.com>
2019-03-18 13:09:07 +0530

    implented SpringDataJPA using JpaRepository class

--- src/main/java/com/example/database/databasedemo/SpringDataDemoApplication.java
+++ src/main/java/com/example/database/databasedemo/SpringDataDemoApplication.java
@@ -12,17 +12,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.database.databasedemo.entity.Person;
 import com.example.database.databasedemo.jdbc.PersonJdbcDao;
 import com.example.database.databasedemo.jpa.PersonJpaRepository;
+import com.example.database.databasedemo.springdata.PersonSpringDataRepository;
 
-//@SpringBootApplication
-public class JPADemoApplication implements CommandLineRunner {
+@SpringBootApplication
+public class SpringDataDemoApplication implements CommandLineRunner {
 	
-	private Logger logger=LoggerFactory.getLogger(JPADemoApplication.class);
+	private Logger logger=LoggerFactory.getLogger(SpringDataDemoApplication.class);
 	
 	@Autowired
-	PersonJpaRepository repository;
+	PersonSpringDataRepository repository;
 	
 	public static void main(String[] args) {
-		SpringApplication.run(JPADemoApplication.class, args);
+		SpringApplication.run(SpringDataDemoApplication.class, args);
 	}
 
 	@Override
@@ -30,10 +31,10 @@ public class JPADemoApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 
 		logger.info("Users details of 10001  -> {}",repository.findById(10001));		
-		logger.info("updating 10003 -> {}",repository.update(new Person(10003,"Sam","Chennai",new Date())));
+		logger.info("updating 10003 -> {}",repository.save(new Person(10003,"Sam","Chennai",new Date())));
 //		logger.info("Inserting 10004 -> {}",repository.update(new Person(10004,"Samar","Mumbai",new Date())));
-		logger.info("Inserting 10004 -> {}",repository.update(new Person("Amar","Mumbai",new Date())));
-		logger.info("Inserting 10004 -> {}",repository.update(new Person("Amar","Mumbai",new Date())));
+		logger.info("Inserting 10004 -> {}",repository.save(new Person("Amar","Mumbai",new Date())));
+		logger.info("Inserting 10004 -> {}",repository.save(new Person("Amar","Mumbai",new Date())));
 		repository.deleteById(10002);
 		logger.info("All users -> {}",repository.findAll());
 		//		logger.info("Deleting 10002 -> No of rows deleted -> {}",dao.deleteById(10002));
